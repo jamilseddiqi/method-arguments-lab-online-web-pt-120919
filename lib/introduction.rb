51a06eb9733f1eb3fd3d6cbf06223608abace0df
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-11 19:12:36 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def greeting(name)
+  puts "Hello #{name}"
+end
+greeting(Jamil)
+
+def introduction_with_language(name, language)
+  puts "Hello my name is #{name} and I am learning to program in #{{language}}"
+end
+introduction_with_language("Jamil", "Ruby")
